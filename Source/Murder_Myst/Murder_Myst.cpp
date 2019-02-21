c5d30cb09ffd638af239c4d9e713d86fd28f4b97
CYT-SubZer0 <cyt.subzer0@gmail.com>
2019-02-21 18:19:04 -0500

    Initial Push
    Start of the game.

---
+++ Source/Murder_Myst/Murder_Myst.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Murder_Myst.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Murder_Myst, "Murder_Myst" );
---
+++ Source/Murder_Myst/Murder_MystGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Murder_MystGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MURDER_MYST_API AMurder_MystGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
